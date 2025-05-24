b04cb1792d3fcc55ace644cce5a5efcd5421001d
xn <46943891+sam-xn@users.noreply.github.com>
2025-05-24 10:38:33 -0400

    styling

--- client/src/components/Contacts.jsx
+++ client/src/components/Contacts.jsx
@@ -1,37 +1,45 @@
 export default function Contacts({ company, contacts }) {
 
+    const button_classname = "grid text-center text-sm font-medium shadow-xs ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-slate-500 border-input bg-background hover:bg-[#DEE1F4] rounded-md py-2";
+    console.log(contacts)
     return (
         <>
             <div className="max-w-full m-4 p-8 bg-[#eff1fc] rounded shadow border border-slate-500">
 
                 <div className="p-1 mb-8 text-[#544B76] font-bold text-xl border-b border-slate-500">
-                    {/*{params.company}*/}
                     {company}
                 </div>
 
-                <div className="flex gap-2 my-4 text-[#544B76]">
+                <div className="flex gap-2 my-4 text-[#544B76] text-lg">
                     <div className="mx-4 text-lg"> Primary Contact: </div>
-                    <div className="pt-1">{contacts.primary?.name ? contacts.primary.name : ''}</div>
-                    <div className="pt-1"> | </div>
-                    <div className="pt-1">{contacts.primary?.email ? contacts.primary.email : ''}</div>
+                    <div>{contacts.primary?.name ? contacts.primary.name : ''}</div>
+                    <div> | </div>
+                    <div>{contacts.primary?.email ? contacts.primary.email : ''}</div>
                 </div>
-                <div className="flex gap-2 py-4 text-[#544B76]">
-                    <div className="mx-4 text-lg"> Accounting Contact: </div>
-                    <div className="pt-1">{contacts.accounting?.name ? contacts.accounting.name : ''}</div>
-                    <div className="pt-1"> | </div>
-                    <div className="pt-1">{contacts.accounting?.email ? contacts.accounting.email : ''}</div>
-                </div>
-                <div className="flex gap-2 my-4 text-[#544B76]">
-                    <div className="mx-4 text-lg"> Other Contacts: </div>
-                    {contacts.other?.map(() => {
-                        <>
-                            <div className="pt-1">{contacts.other.name}</div>
-                            <div className="pt-1"> | </div>
-                            <div className="pt-1">{contacts.other.email}</div>
-                        </>
-                    })}
-                </div>
-
+                {contacts.accounting
+                    ?
+                    <div className="flex gap-2 py-4 text-[#544B76] text-lg">
+                        <div className="mx-4 text-lg"> Accounting Contact: </div>
+                        <div>{contacts.accounting?.name ? contacts.accounting.name : ''}</div>
+                        <div> | </div>
+                        <div>{contacts.accounting?.email ? contacts.accounting.email : ''}</div>
+                    </div>
+                    : <></>
+                }
+                {contacts.other[0]
+                    ? <>
+                        <div className="flex gap-2 my-4 text-[#544B76] text-lg">
+                            <div className="mx-4 text-lg"> Other Contacts: </div>
+                            {contacts.other?.map(other => {
+                                <>
+                                    <div>{other?.name}</div>
+                                    <div> | </div>
+                                    <div>{other?.email}</div>
+                                </>
+                            })}
+                        </div>
+                    </> : <></>
+                }
             </div>
         </>
     );
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,4 +1,5 @@
 import Sidebar from '/src/components/Sidebar';
+import { HardDrive } from 'lucide-react'
 import { Heart } from 'lucide-react'
 
 export default function Home() {
@@ -9,14 +10,14 @@ export default function Home() {
 				<div className=""><Sidebar /></div>
 				<div className="col-span-5 h-full align-items-center">
 					<div className={div_classname + " bg-[#eff1fc] text-[#544B76] font-semibold h-full"}>
-						<div className="grid grid-rows-3 gap-8">
+						<div className="grid grid-rows-4 gap-8">
 							<div></div>
-							<div className="flex gap-8 text-4xl mx-12 my-4">
-								<p>Waterloo Eletro..</p><p>- less system, more plating -</p>
-							</div>
-							<div className="mx-24 text-3xl flex gap-12">
-								<p> but here's a database, in case it helps.</p>
-								<div className="flex gap-2"> <Heart className="mt-3 mx-1"/> sam </div>
+							<div className="flex gap-8 text-4xl mx-12 my-8"><HardDrive className="" size={46} /> Waterloo Electro System </div>
+							{/*<div className="text-4xl grid grid-cols-3"><div></div><p className="col-span-2">welcome to your database -</p></div>*/}
+							<div className="mt-12 text-3xl tracking-wide grid grid-cols-5">
+							<div></div>
+								<p className="col-span-2"> welcome to your database.</p>
+								{/*<div className="flex gap-2"> -<Heart className="mt-3 mx-1" /> sam </div>*/}
 							</div>
 						</div>
 					</div>
