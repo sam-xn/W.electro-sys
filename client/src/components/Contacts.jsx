b19cf2de3d987f369d6384b497de4478b0a37ddb
xn <46943891+sam-xn@users.noreply.github.com>
2025-06-16 14:48:30 -0400

    new contact functional redesign

--- client/src/components/Contacts.jsx
+++ client/src/components/Contacts.jsx
@@ -10,7 +10,6 @@ export default function Contacts({ company }) {
     useEffect(() => {
         ContactService.getCompany(company)
             .then((response) => {
-                console.log(response.data)
                 const d = response.data;
                 const c = { primary: [], accounting: [], other: [] };
                 d.forEach(r => {
@@ -39,7 +38,7 @@ export default function Contacts({ company }) {
                     }
                 });
 
-                setContacts(c); console.log(c)
+                setContacts(c); 
             })
             .catch((e) => {
                 console.log(e);
@@ -47,7 +46,7 @@ export default function Contacts({ company }) {
     }, []);
 
     const button_classname = "grid text-center text-sm font-medium shadow-xs ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-slate-500 border-input bg-background hover:bg-[#DEE1F4] rounded-md py-2";
-    const link_classname = "hover:text-white mx-2 px-2 pt-1 rounded text-sm text-center bg-[#DEE1F4] outline hover:bg-blue-800";
+    const link_classname = "hover:text-white mx-2 px-2 my-2 rounded text-sm text-center bg-[#DEE1F4] outline hover:bg-blue-800";
 
     return (
         <>
@@ -57,73 +56,58 @@ export default function Contacts({ company }) {
                     {company}
                 </div>
 
-                <div className="flex gap-2 my-4 text-[#544B76] text-lg">
-                    {contacts.primary[0]?.id ?
-                        <>
-                            <div className="mx-4 text-sm "> Primary: </div>
-                            <div>{contacts.primary[0].name}</div>
-                            {contacts.primary.email !== null ?
-                                <div className="border-l border-slate-500 pl-2 ml-2">{contacts.primary.email}</div>
-                                : <></>
-                            }
-                            {contacts.primary.phone !== null ?
-                                <div className="border-l border-slate-500 pl-2 ml-2">{contacts.primary.phone}</div>
-                                : <></>
-                            }
-                            <Link
-                                className={link_classname}
-                                to={`/contacts/${contacts.primary.id}/edit`}>
-                                Edit
-                            </Link>
-                        </> : <></>
-                    }
-                </div>
-                {/*<div className="flex gap-2 my-4 text-[#544B76] text-lg">*/}
-                {/*    {contacts.accounting?[0].id ?*/}
-                {/*    <>*/}
-                {/*        <div className="mx-4 text-lg"> Accounting: </div>*/}
-                {/*        <div>{contacts.accounting.name}</div>*/}
-                {/*        {contacts.accounting.email !== null ?*/}
-                {/*            <div className="border-l border-slate-500 pl-2 ml-2">{contacts.accounting.email}</div>*/}
-                {/*            : <></>*/}
-                {/*        }*/}
-                {/*        {contacts.accounting.phone !== null ?*/}
-                {/*            <div className="border-l border-slate-500 pl-2 ml-2">{contacts.accounting.phone}</div>*/}
-                {/*            : <></>*/}
-                {/*        }*/}
-                {/*        <Link*/}
-                {/*            className={link_classname}*/}
-                {/*            to={`/contacts/${contacts.accounting.id}/edit`}>*/}
-                {/*            Edit*/}
-                {/*        </Link> */}
-                {/*        </> : <></>*/}
-                {/*    }*/}
-                {/*</div>*/}
-                {/*<div className="flex gap-2 my-4 text-[#544B76] text-lg">*/}
-                {/*    {contacts.other ? <>*/}
-                {/*        {contacts.other[0] !== null*/}
-                {/*            ? <>*/}
-                {/*                <div className="flex gap-2 my-4 text-[#544B76] text-lg">*/}
-                {/*                    <div className="mx-4 text-lg"> Other: </div>*/}
-                {/*                    {contacts.other.map((other, i) => {*/}
-                {/*                        <>*/}
-                {/*                            <div>{other[i].name}</div>*/}
-                {/*                            <div> | </div>*/}
-                {/*                            <div>{other[i].email}</div>*/}
-                {/*                            {contacts.other[i].id ?*/}
-                {/*                                <Link*/}
-                {/*                                    className={link_classname}*/}
-                {/*                                    to={`/contacts/${contacts.other[i].id}/edit`}>*/}
-                {/*                                    Edit*/}
-                {/*                                </Link>*/}
-                {/*                            : <></>}*/}
-                {/*                        </>*/}
-                {/*                    })}*/}
-                {/*                </div>*/}
-                {/*            </> : <></>*/}
-                {/*        }*/}
-                {/*    </> : <></>}*/}
-                {/*</div>*/}
+                {contacts.primary[0]?.id ?
+                    <>
+                        <div className="text-[#544B76] underline my-2 mx-4"> Primary </div>
+                        {contacts.primary.map(c => <>
+                            <div className="ml-8 mr-4 flex justify-between text-[#544B76] border-b border-slate-500">
+                                <div className="px-2 pt-1">{c.name}</div>
+                                {c.email !== null ? <div className="px-2 pt-1">{c.email}</div> : <></>}
+                                {c.phone !== null ? <div className="px-2 pt-1">{c.phone}</div> : <></>}
+                                <Link
+                                    className={link_classname}
+                                    to={`/contacts/${c.id}/edit`}>
+                                    Edit
+                                </Link>
+                            </div>
+                        </>)}
+                    </> : <></>
+                }
+                {contacts.accounting[0]?.id ?
+                    <>
+                        <div className="text-[#544B76] underline my-2 mx-4"> Accounting </div>
+                        {contacts.accounting.map(c => <>
+                            <div className="ml-8 mr-4 flex justify-between text-[#544B76] border-b border-slate-500">
+                                <div className="px-2 pt-1">{c.name}</div>
+                                {c.email !== null ? <div className="px-2 pt-1">{c.email}</div> : <></>}
+                                {c.phone !== null ? <div className="px-2 pt-1">{c.phone}</div> : <></>}
+                                <Link
+                                    className={link_classname}
+                                    to={`/contacts/${c.id}/edit`}>
+                                    Edit
+                                </Link>
+                            </div>
+                        </>)}
+                    </> : <></>
+                }
+                {contacts.other[0]?.id ?
+                    <>
+                        <div className="text-[#544B76] underline my-2 mx-4"> Other </div>
+                        {contacts.other.map(c => <>
+                            <div className="ml-8 mr-4 flex justify-between text-[#544B76] border-b border-slate-500">
+                                <div className="px-2 pt-1">{c.name}</div>
+                                {c.email !== null ? <div className="px-2 pt-1">{c.email}</div> : <></>}
+                                {c.phone !== null ? <div className="px-2 pt-1">{c.phone}</div> : <></>}
+                                <Link
+                                    className={link_classname}
+                                    to={`/contacts/${c.id}/edit`}>
+                                    Edit
+                                </Link>
+                            </div>
+                        </>)}
+                    </> : <></>
+                }
+
             </div>
         </>
     );
---
+++ client/src/components/contacts/EditContact.jsx
@@ -0,0 +1,220 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Sidebar from '/src/components/Sidebar';
+
+import CustomerService from '/src/components/http/customer.service';
+
+import Error from '/src/components/Error';
+
+function EditContact() {
+    // ---------------------------------------------------- ErrorModal 
+    const [error, setError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+    function handleClose() { setError(false); }
+    // --------------------------------------------------- /ErrorModal 
+    const navigate = useNavigate();
+    const [submitted, setSubmitted] = useState(false);
+
+    const [customer, setCustomer] = useState("select");
+    const [customerList, setCustomerList] = useState([]);
+
+    const [newCompany, setNewCompany] = useState("");
+    const [newContactType, setNewContactType] = useState("primary");
+    const [newContactName, setNewContactName] = useState("");
+    const [newContactEmail, setNewContactEmail] = useState("");
+    const [newContactPhone, setNewContactPhone] = useState("");
+
+    useEffect(() => {
+        CustomerService.getAll()
+            .then((response) => {
+                setCustomerList(response.data.map((customer) => customer.company));
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    }, []);
+
+    function saveContact() {
+
+        if (customer === "select") {
+            setErrorMessage("Please select an existing customer or select New Customer to create a new contact.");
+            setError(true);
+            return;
+        }
+
+        else if (customer === "new") {
+            if (newCompany === "") {
+                setErrorMessage("Please enter new Company Name.");
+                setError(true);
+                return;
+            }
+            else if (newContactName === "" || (newContactEmail === "" && newContactPhone === "")) {
+                setErrorMessage("Please enter new Contact info.");
+                setError(true);
+                return;
+            }
+
+            CustomerService.create({ company: newCompany.toUpperCase(), name: newContactName, email: newContactEmail, phone: newContactPhone, type: newContactType })
+                .then(() => {
+                    setSubmitted(true);
+                })
+                .catch((e) => {
+                    console.log(e);
+                });
+        }
+
+        else {
+            if (newContactName === "" || (newContactEmail === "" && newContactPhone === "")) {
+                setErrorMessage("Please enter new Contact info.");
+                setError(true);
+                return;
+            }
+
+            const info = { company: customer, name: newContactName, type: newContactType };
+            if (newContactEmail !== "") info.email = newContactEmail;
+            if (newContactPhone !== "") info.phone = newContactPhone;
+
+            ContactService.create(info)
+                .then(() => {
+                    setSubmitted(true);
+                })
+                .catch((e) => {
+                    console.log(e);
+                });
+        }
+    }
+
+    const label_classname = "font-bold text-md text-[#544B76] pl-4 pt-4";
+    const input_classname = "focus:outline-none border-b border-slate-500 text-center col-span-3 pr-16";
+
+    return (
+        <>
+            <div className="grid grid-cols-6">
+                <div><Sidebar /></div>
+                <div className="col-span-5">
+                    <div className="grid place-items-center">
+                        {error ? <Error isOpen={error} onClose={handleClose}> {errorMessage} </Error> : <></>}
+
+                        <div className="max-w-3/4 mx-4 py-8 px-8 mb-12 bg-[#eff1fc] rounded shadow border border-slate-500">
+
+                            <div className="p-1 mb-8 text-[#544B76] font-bold text-xl border-b border-slate-500">
+                                New Contact
+                            </div>
+
+                            <div className="bg-white grid grid-cols-4 m-4 pl-8 py-4 max-w-full border border-slate-500">
+                                <div className={label_classname}> Customer: </div>
+                                <select
+                                    className="col-span-3 mr-8 border-b border-slate-500 text-center"
+                                    defaultValue="select"
+                                    onChange={(e) => { setCustomer(e.target.value); if (e.target.value === "new") setNewContactType("primary"); else setNewContactType("other"); }}
+                                >
+                                    <option value="select"> {"< select customer >"} </option>
+                                    <option value="new"> New Customer </option>
+                                    {customerList.map((company, index) =>
+                                        <option value={company}> {company} </option>
+                                    )}
+                                </select>
+
+                                {customer !== "select"
+                                    ? (
+                                        <div className="mt-6 mr-8 col-span-4">
+
+                                            {customer == "new" ? <>
+                                                <div className="grid grid-cols-4 mb-4">
+                                                    <div className={label_classname}> Company Name: </div>
+                                                    <input className={input_classname}
+                                                        type="text"
+                                                        value={newCompany}
+                                                        onChange={(e) => setNewCompany(e.target.value)}
+                                                    />
+                                                </div>
+                                            </> : <></>}
+
+                                            <div className="grid grid-cols-4 mt-8">
+                                                <div className="text-[#544B76] font-bold ml-4 mb-2"> Type: </div>
+                                                <div>
+                                                    <input
+                                                        type="radio"
+                                                        name="type"
+                                                        value="primary"
+                                                        checked={newContactType === "primary"}
+                                                        onChange={(e) => setNewContactType(e.target.value)}
+                                                    />
+                                                    <label className="text-md px-2"> Primary </label>
+                                                </div>
+                                                <div>
+                                                    <input
+                                                        type="radio"
+                                                        name="type"
+                                                        value="accounting"
+                                                        onChange={(e) => setNewContactType(e.target.value)}
+                                                    />
+                                                    <label className="text-md px-2"> Accounting </label>
+                                                </div>
+                                                <div>
+                                                    <input
+                                                        type="radio"
+                                                        name="type"
+                                                        value="other"
+                                                        checked={newContactType === "other"}
+                                                        onChange={(e) => setNewContactType(e.target.value)}
+                                                    />
+                                                    <label className="text-md px-2"> Other </label>
+                                                </div>
+                                            </div>
+
+                                            <div className="grid grid-cols-4">
+
+                                                <div className={label_classname}> Contact Name: </div>
+                                                <input className={input_classname}
+                                                    type="text"
+                                                    value={newContactName}
+                                                    onChange={(e) => setNewContactName(e.target.value)}
+                                                />
+
+                                                <div className={label_classname}> Contact Email: </div>
+                                                <input className={input_classname}
+                                                    type="text"
+                                                    value={newContactEmail}
+                                                    onChange={(e) => setNewContactEmail(e.target.value)}
+                                                />
+                                                <div className={label_classname}> Contact Phone: </div>
+                                                <input className={input_classname}
+                                                    type="text"
+                                                    value={newContactPhone}
+                                                    onChange={(e) => setNewContactPhone(e.target.value)}
+                                                />
+                                            </div>
+
+                                            <div className="mt-4 mr-8 text-center text-sm text-slate-600">
+                                                <i>* please confirm spelling before submitting.</i>
+                                            </div>
+                                        </div>
+                                    )
+                                    : <></>
+                                }
+                            </div>
+
+                            <div className="pt-4 mx-8 flex gap-4 place-items-center">
+                                <button
+                                    className="text-white mb-2 mx-4 py-1 rounded w-sm bg-[#544B76] outline hover:bg-red-800"
+                                    onClick={() => navigate(`/customers`)}
+                                >
+                                    Discard
+                                </button>
+                                <button
+                                    className="text-white mb-2 py-1 rounded w-sm bg-[#544B76] outline hover:bg-blue-800"
+                                    onClick={saveContact}
+                                >
+                                    Submit
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default EditContact;
